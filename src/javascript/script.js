5a67478e5137247b3d0202e0a02d9e9b425daedd
Gerson Junior <gerson_online2@hotmail.com>
2024-03-24 16:15:16 -0300

    criado paginas home e sobre

---
+++ src/javascript/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#mobile_btn').on('click', function(){
+        $('#mobile_menu').toggleClass('active')
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
+});
+
